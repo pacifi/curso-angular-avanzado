d01cde651a24485872c9942f4836a60eaaafb987
Bonnier Nilss Mamani Larico <pacifi.bnr@gmail.com>
2025-07-24 03:12:07 -0500

    prettier config

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -3,12 +3,10 @@ import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private http = inject(HttpClient);
-
 
   getProducts(category_id?: string) {
     const url = new URL(`https://api.escuelajs.co/api/v1/products`);
@@ -19,6 +17,8 @@ export class ProductService {
   }
 
   getOne(id: string) {
-    return this.http.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`);
+    return this.http.get<Product>(
+      `https://api.escuelajs.co/api/v1/products/${id}`,
+    );
   }
 }
