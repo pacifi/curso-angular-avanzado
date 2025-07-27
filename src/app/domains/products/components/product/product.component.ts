35f9115a4ae5c7fed1adfe00aefd28fb9176d82c
pacifi <pacifi.bnr@gmail.com>
2025-07-27 14:02:26 -0500

    computed

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { Product } from '@shared/models/product.model';
@@ -14,7 +14,7 @@ export class ProductComponent {
   // eslint-disable-next-line @angular-eslint/no-input-rename
   readonly $product = input.required<Product>({ alias: 'product' });
 
-  @Output() addToCart = new EventEmitter();
+  readonly addToCart = output<Product>();
 
   addToCartHandler() {
     this.addToCart.emit(this.$product());
